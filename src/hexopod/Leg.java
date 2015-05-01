3e3760600634f1fbfe5c03408008f815e02b667b
root <root@firewall-laptop>
2015-05-01 09:14:35 +0200

    v2 drie poten in body. ps: leg & base zitten in verkeerde volgorde. dat wordt nog aangepast in volgende versie

--- src/hexopod/Leg.java
+++ src/hexopod/Leg.java
@@ -18,7 +18,16 @@ public class Leg extends Node {
     private Geometry leg;   //beenonderdeel :Onderbeen
     private Geometry ankle; //servo         :enkel
     private Geometry base;  //beenonderdeel :voet
+    
     private Node hipNode;
+    private Node kneeNode;
+    private Node ankleNode;
+    private Node thighNode;
+    private Node legNode;
+    private Node baseNode;
+
+    
+    
     
     private int id;
     private AssetManager assetManager;
@@ -29,29 +38,35 @@ public class Leg extends Node {
         
         switch (id) {
             case 1:
-                initHip();
                 break;
             case 2:
-                initThigh();
                 break;
             case 3:
-                initKnee();
                 break;
             case 4:
-                initLeg();
                 break;
             case 5:
-                initAnkle();
                 break;
             case 6:
-                initBase();
                 break;
+                default:
+                    
         }
-     //   setupParts();
+      
         
 
     }
-    
+    public Leg(AssetManager assetmanager){
+        assetManager = assetmanager;
+                initNodes();
+                initHip();
+                initThigh();
+                initKnee();
+                initLeg();
+                initAnkle();
+                initBase();
+                setupParts();                                    
+    }
     /*
      * NOTES
      * *******
@@ -62,25 +77,31 @@ public class Leg extends Node {
      * 
      */
     
+    private void initNodes() {
+        
+        ankleNode = new Node();
+        baseNode = new Node();
+    }
+    
     private void setupParts() {
         
-        //Rotation
-      //  hip.rotate(FastMath.HALF_PI, 0, 0);
-          thigh.rotate(FastMath.PI,1.5f,0);
-        //Translation
-        hip.setLocalTranslation(-0.1f, 0.2f, 0);
-        thigh.setLocalTranslation(new Vector3f(-0.3f, 0.2f, 0f));
-    //    knee.setLocalTranslation(new Vector3f(0, 1f,0.15f));
+        hipNode.setLocalTranslation(-0.1f, 0, 0);
+       kneeNode.rotate(0,0, -1f); 
+        hipNode.rotate(0, 0, 0f);
+        this.attachChild(hipNode);
     }
 
     private void initHip() {
         Cylinder smallCylinder = new Cylinder(20, 20, 0.07f, 0.1f, true);
         hip = new Geometry("Hip", smallCylinder);
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.BlackNoAlpha);
+        mat.setColor("Color", ColorRGBA.Red);
         hip.setMaterial(mat);
-        hip.rotate(FastMath.HALF_PI, 0, 0);
-        this.attachChild(hip);
+        hipNode = new Node();
+        hipNode.attachChild(hip);
+        hipNode.setLocalTranslation(-0.1f, 0, 0);
+        hipNode.rotate(FastMath.HALF_PI, 0f, 0);
+        hipNode.rotate(0, 0, -1.6f);
     }
     
     private void initThigh() {
@@ -89,45 +110,61 @@ public class Leg extends Node {
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Brown);
         thigh.setMaterial(mat);
-        this.attachChild(thigh);
+        thighNode = new Node();
+        thighNode.attachChild(thigh);
+        thighNode.rotate(FastMath.HALF_PI, 0, 0);
+        thighNode.setLocalTranslation(0, -0.15f,0); //.,X,y
+        hipNode.attachChild(thighNode);
     }
     
     private void initKnee() {
         Cylinder smallCylinder = new Cylinder(20, 20, 0.07f, 0.1f, true);
         knee = new Geometry("Knee", smallCylinder);
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.BlackNoAlpha);
+        mat.setColor("Color", ColorRGBA.Red);
         knee.setMaterial(mat);
-        knee.rotate(0, 1.6f,2f);
-        this.attachChild(knee);
+        kneeNode = new Node();
+        kneeNode.attachChild(knee);
+        kneeNode.rotate(FastMath.HALF_PI, 0, 0);
+        kneeNode.rotate(0, 1.4f, 0);
+        kneeNode.setLocalTranslation(0f, 0, 0.15f);
+        thighNode.attachChild(kneeNode);
     }
     
     private void initLeg() {
-        Cylinder cylinder = new Cylinder(20, 20, 0.03f, 0.40f, true);
+        Cylinder cylinder = new Cylinder(20, 20, 0.03f, 0.50f, true);
         leg = new Geometry("Leg", cylinder);
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Brown);
+        mat.setColor("Color", ColorRGBA.Blue);
         leg.setMaterial(mat);
-        this.attachChild(leg);
+        legNode = new Node();
+        legNode.attachChild(leg);
+        legNode.rotate(FastMath.HALF_PI, 0, 0);
+        legNode.setLocalTranslation(-0.6f, 0.2f, 0);
+        kneeNode.attachChild(legNode);
     }
     
     private void initAnkle() {
         Cylinder smallCylinder = new Cylinder(20, 20, 0.07f, 0.1f, true);
         ankle = new Geometry("Ankle", smallCylinder);
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.BlackNoAlpha);
+        mat.setColor("Color", ColorRGBA.Red);
         ankle.setMaterial(mat);
-        ankle.rotate(0, 1.6f,2f);
-        this.attachChild(ankle);
+        ankleNode.attachChild(ankle);
+        ankleNode.rotate(0, 1.6f,2f);
+        ankleNode.setLocalTranslation(0, 0, 0.20f);
+        legNode.attachChild(ankleNode);
     }
     
     private void initBase() {
         Cylinder cylinder = new Cylinder(20, 20, 0.03f, 0.60f, true);
         base = new Geometry("Base", cylinder);
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Brown);
+        mat.setColor("Color", ColorRGBA.Green);
         base.setMaterial(mat);
-        this.attachChild(base);
+        baseNode.attachChild(base);
+        baseNode.setLocalTranslation(0, 0, 0.3f);
+        ankleNode.attachChild(baseNode);
     }
 
 }
