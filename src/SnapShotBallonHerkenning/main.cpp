f4c43073ab4dc616b8e1d124fda7618a9150a04c
aman1300 <aman1300@student.nhl.nl>
2015-05-26 11:21:28 +0200

    nu met afstand berekening

--- src/SnapShotBallonHerkenning/main.cpp
+++ src/SnapShotBallonHerkenning/main.cpp
@@ -43,21 +43,38 @@ void drawObject(int x, int y, Mat &frame) {
     cv::putText(frame, intToString(x) + " , " + intToString(y), cv::Point(x, y + 20), 1, 1, Scalar(0, 255, 0));
 }
 
+int getDistance(int hoogteObjectInPx) {
+    int brandpuntAfstandMM = 29;
+    int hoogteObjectInMM = 27; // nog op te meten 
+
+    hoogteObjectInPx = hoogteObjectInPx;
+    int relatieveAfstand = 0;
+
+    return relatieveAfstand = (((hoogteObjectInPx - hoogteObjectInMM) * brandpuntAfstandMM) / hoogteObjectInMM);
+}
+
 Mat redFilter(Mat& src) {
     assert(src.type() == CV_8UC3);
 
     //// voor rood, met veel licht
-    //    int H_MIN = 0;
+    //        int H_MIN = 0;
+    //        int H_MAX = 256;
+    //        int S_MIN = 141;
+    //        int S_MAX = 255;
+    //        int V_MIN = 137;
+    //        int V_MAX = 255;
+    //    int H_MIN = 163;
     //    int H_MAX = 256;
-    //    int S_MIN = 141;
+    //    int S_MIN = 47;
     //    int S_MAX = 255;
-    //    int V_MIN = 137;
+    //    int V_MIN = 0;
     //    int V_MAX = 255;
-    int H_MIN = 163;
-    int H_MAX = 256;
-    int S_MIN = 47;
-    int S_MAX = 255;
-    int V_MIN = 0;
+
+    int H_MIN = 0;
+    int H_MAX = 5;
+    int S_MIN = 140;
+    int S_MAX = 253;
+    int V_MIN = 65;
     int V_MAX = 255;
 
     Mat threshRed, thresholded;
@@ -65,15 +82,16 @@ Mat redFilter(Mat& src) {
     inRange(threshRed, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), thresholded);
 
     //create structuring element that will be used to "dilate" and "erode" image.
-    //the element chosen here is a 3px by 3px rectangle
-    Mat erodeElement = getStructuringElement(MORPH_RECT, Size(20, 20));
+    //the element chosen here is a 3px by 3px rectangle 
+    Mat erodeElement = getStructuringElement(MORPH_RECT, Size(3, 3));
 
-    //dilate with larger element so make sure object is nicely visible
-    Mat dilateElement = getStructuringElement(MORPH_RECT, Size(18, 18));
+    //dilate with larger element so make sure object is nicely visible 
+    Mat dilateElement = getStructuringElement(MORPH_RECT, Size(8, 8));
 
     erode(thresholded, thresholded, erodeElement);
     erode(thresholded, thresholded, erodeElement);
 
+    dilate(thresholded, thresholded, dilateElement);
     dilate(thresholded, thresholded, dilateElement);
 
     return thresholded;
@@ -112,34 +130,19 @@ Mat blueFilter(const Mat& src) {
 
     return thresholded;
 }
+//int &x, int &y, Mat threshold, Mat &camerFeed
 
-Mat getContour(Mat &thresholdFilter) {
+void getContour(int &x, int &y, Mat threshold, Mat &thresholdFilter) {
 
-    int x, y;
+    Mat temp;
+    threshold.copyTo(temp);
     /// Find contours   
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
-    RNG rng(12345); // optional, random number generator 
+    //RNG rng(12345); // optional, random number generator 
 
-    findContours(thresholdFilter, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-    /// Find the rotated ellipses for each contour
-    vector<RotatedRect> minEllipse(contours.size());
-
-    for (int i = 0; i < contours.size(); i++) {
-        if (contours[i].size() > 5) {
-            minEllipse[i] = fitEllipse(Mat(contours[i]));
-        }
-    }
-
-    /// Draw contours
-    Mat drawing = Mat::zeros(thresholdFilter.size(), CV_8UC3);
-    for (int i = 0; i < contours.size(); i++) {
-        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        //drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
-        // ellipse
-        ellipse(drawing, minEllipse[i], color, 2, 8);
-    }
+    //findContours(thresholdFilter, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    findContours(temp, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE); // retrieves external 
 
     //use moments method to find our filtered object
     double refArea = 0;
@@ -157,79 +160,57 @@ Mat getContour(Mat &thresholdFilter) {
                 //if the area is the same as the 3/2 of the image size, probably just a bad filter
                 //we only want the object with the largest area so we safe a reference area each
                 //iteration and compare it to the area in the next iteration.
-                if (area > MIN_OBJECT_AREA) {
+                if (area > MIN_OBJECT_AREA && area < MAX_OBJECT_AREA && area > refArea) {
                     x = moment.m10 / area;
                     y = moment.m01 / area;
-
-                    cout << x << endl;
-                    cout << y << endl;
                     objectFound = true;
+                    refArea = area;
+                    
+                    cout << "x:" << x << endl;
+                    cout << "y:" << y << endl;
+                    objectFound = true;
+
+                    int relativeDistance = getDistance(x);
+
+                    cout << "afstand object:" + intToString(relativeDistance) << endl;
 
                 } else objectFound = false;
             }
             //let user know you found an object
             if (objectFound == true) {
                 //draw object location on screen
-                drawObject(x, y, drawing);
+                drawObject(x, y, thresholdFilter);
             }
-        } //else putText(cameraFeed, "TOO MUCH NOISE! ADJUST FILTER", Point(0, 50), 1, 2, Scalar(0, 0, 255), 2);
-    }
-
-    //// for debug only
-    imshow("Result window", drawing);
-
-    return thresholdFilter;
+        } 
+    } 
 }
 
 bool getVideo(Mat &vidframe) {
 
+    bool recording = false;
+    bool startNewRecording = false;
+
     VideoCapture videoConnection(0);
-    
+
     if (!videoConnection.isOpened()) // if not success, exit program
     {
         cout << "ERROR COULD NOT CONNECT WITH WEBCAM!" << endl;
         return false;
     }
-    
+
     VideoWriter writer; // Write each frame from the webcam to the matrix.
     //foto-options
     int fcc = CV_FOURCC('J', 'P', 'E', 'G'); // four character code.
     int fps = 1; // frame per second interger. 
     Size framesize(videoConnection.get(CV_CAP_PROP_FRAME_WIDTH), videoConnection.get(CV_CAP_PROP_FRAME_HEIGHT)); // frame size
 
-    //waitKey(3000);
-    string filePath = "img//SNAPSHOT.jpeg"; // imagePath
-    writer = VideoWriter(filePath, fcc, fps, framesize); //Writes the jpg-file to a path.
-
-    if (!writer.isOpened()) {
-        cout << "Error opening file for writer" << endl;
-        getchar;
-        return false;
-    }
-
-    bool bSuccess = videoConnection.read(vidframe); //Check if types are OK.
-
-    if (!bSuccess) //test if frame successfully read
-    {
-        cout << "ERROR READING FRAME FROM CAMERA FEED" << endl;
-        return false;
-    }
-
-
-    writer.write(vidframe);
-
-    /*
     int breakLoop = 0;
-    
-    while (1) {
 
-        //startNewRecording = true;
+    while (1) {
 
         if (startNewRecording == true) {
             startNewRecording = false;
             recording = true;
-
-            videoNumber++; // when new recording is started, file name will be incremented
 
             string filePath = "img//SNAPSHOT.jpeg"; // imagePath
             writer = VideoWriter(filePath, fcc, fps, framesize); //Writes the jpg-file to a path.
@@ -252,26 +233,21 @@ bool getVideo(Mat &vidframe) {
         if (recording == true) // are recording
         {
             // write each frame from the webcam
-            writer.write(vidframe); 
+            writer.write(vidframe);
         }
-        
-        if (waitKey(2000)) {
+
+        if (waitKey(200)) {
 
             startNewRecording = true;
         }
 
         //// after ten snapshots the wile loop
         //// will be broken
-        if (breakLoop == 10) {
+        if (breakLoop == 3) {
             break;
         }
         breakLoop++;
     }
-     */ 
-
-
-
-
     return true;
 }
 
@@ -281,21 +257,13 @@ int main(int argc, char* argv[]) {
 
     //Matrix to store each frame of the webcam feed
     Mat cameraFeed;
-    Mat cameraFrame;
-    Mat threshold;
-    Mat HSV;
+    Mat cameraFrame; 
 
-    //video capture object to acquire webcam feed
-    //    VideoCapture capture;
-    //open capture object at location zero (default location for webcam)
-    //    capture.open(0);
-    //set height and width of capture frame
-    //    capture.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
-    //    capture.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
+    int x = 0, y = 0; 
+    
     //start an infinite loop where webcam feed is copied to cameraFeed matrix
     //all of our operations will be performed within this loop
     while (1) {
-
 
         bool balloon = getVideo(cameraFeed);
 
@@ -318,9 +286,9 @@ int main(int argc, char* argv[]) {
             Mat redOnly = redFilter(cameraFrame);
             Mat gray;
             cvtColor(cameraFrame, gray, CV_BGR2GRAY);
-            Canny(redOnly, gray, 200, 200, 3);
-            Mat contourFound = getContour(gray);
-            imshow("Get Contour", contourFound);
+            Canny(gray, gray, 200, 200, 3);
+            getContour(x, y, redOnly, gray);
+            //imshow("Get Contour", contourFound);
             //inRange(HSV, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), threshold);
             //Mat redOnly = redFilter(threshold);
             //morphOps(threshold);
@@ -335,21 +303,9 @@ int main(int argc, char* argv[]) {
             string src_path1 = "img//gray.jpeg";
             imwrite(src_path1, gray);
 
-            string src_path2 = "img//contourFound.jpeg";
-            imwrite(src_path2, contourFound);
-
             string src_path3 = "img//redOnly.jpeg";
             imwrite(src_path3, redOnly);
         }
-
-        //show frames 
-        //imshow(windowName2,threshold);
-
-        //imshow(windowName, cameraFeed); //for debug only
-        //imshow(windowName1, HSV);
-
-        //delay 30ms so that screen can refresh.
-        //image will not appear without this waitKey() command
         waitKey(30);
     }
     return 0;
